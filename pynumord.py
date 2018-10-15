4fcd9dec7fe63a7b4491784784686ceb189a719c
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-15 08:18:52 -0400

    passing test_scrambled, updated happy and reversed to reflect actual op.

--- pynumord.py
+++ pynumord.py
@@ -8,35 +8,42 @@ def sort_nums(incoming):
         if key == sought:
             yield value
             sought += 1
-            for i in check_store(sought, store_early):
-                yield i
+            for a, b in check_store(sought, store_early):
+                yield a
+                sought = b
+
         else:
             store_early[key] = value
 
 
-def check_store(sought, store):
-    while sought in store:
-        yield store[sought]
-        sought += 1
+
+
+def check_store(seek, store):
+    while seek in store:
+        found = store[seek]
+        del store[seek]
+        seek += 1
+        yield found, seek
 
 
 
-class TestExtremeOrders(unittest.TestCase):
-    def test_happy(self):
-        dict_happy = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
-        array = [i for i in sort_nums(dict_happy)]
-        self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
-
-
+class TestOrders(unittest.TestCase):
     def test_reversed(self):
-        dict_reversed = {10: 'j', 9: 'i', 8: 'h', 7: 'g', 6: 'f',
-                         5: 'e', 4: 'd', 3: 'c', 2: 'b', 1: 'a'}
+        dict_reversed = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
         array = [i for i in sort_nums(dict_reversed)]
         self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
 
 
+    def test_happy(self):
+        dict_happy = {10: 'j', 9: 'i', 8: 'h', 7: 'g', 6: 'f',
+                         5: 'e', 4: 'd', 3: 'c', 2: 'b', 1: 'a'}
+        array = [i for i in sort_nums(dict_happy)]
+        self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+
+
     def test_scrambled(self):
-        dict_scrambled = {3: 'c', 7: 'g', 4: 'd', 9: 'i', 1: 'a', 6: 'f', 2: 'b', 8: 'h', 5: 'e', 10: 'j'}
+        dict_scrambled = {3: 'c', 7: 'g', 4: 'd', 9: 'i', 1: 'a',
+                         6: 'f', 2: 'b', 8: 'h', 5: 'e', 10: 'j'}
         array = [i for i in sort_nums(dict_scrambled)]
         self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
 
