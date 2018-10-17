53ac5d1a099925502f91ce77c5495f15ab688079
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-17 13:21:07 -0400

    Shifted to property-based testing

--- pynumord.py
+++ pynumord.py
@@ -1,9 +1,14 @@
 import unittest
+import hypothesis
+from hypothesis import given, assume
+import hypothesis.strategies as st
 
 def sort_nums(incoming):
     sought = 1
     store_early = {}
+    print("New!!!")
     while len(incoming) > 0:
+        print("Top")
         key, value = incoming.popitem()
         if key == sought:
             yield value
@@ -14,6 +19,7 @@ def sort_nums(incoming):
 
         else:
             store_early[key] = value
+            print("Stored!")
 
 
 
@@ -21,6 +27,7 @@ def sort_nums(incoming):
 def check_store(seek, store):
     while seek in store:
         found = store[seek]
+        print("Check!")
         del store[seek]
         seek += 1
         yield found, seek
@@ -28,36 +35,15 @@ def check_store(seek, store):
 
 
 class TestOrders(unittest.TestCase):
-    def test_reversed(self):
-        dict_reversed = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
-        array = [i for i in sort_nums(dict_reversed)]
-        self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+    @given(value_list = st.lists(st.characters()), integer_list = st.integers(1))  # how can I scramble the dict entries, while knowing what order I want them out in?
+    def test_sorting(self, chars):
+        test_dict = dict(zip(range(len(chars)), chars))
+        if test_dict:
+            del test_dict[0]
+            del chars[0]
 
-
-    def test_happy(self):
-        dict_happy = {10: 'j', 9: 'i', 8: 'h', 7: 'g', 6: 'f',
-                         5: 'e', 4: 'd', 3: 'c', 2: 'b', 1: 'a'}
-        array = [i for i in sort_nums(dict_happy)]
-        self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
-
-
-    def test_scrambled(self):
-        dict_scrambled = {3: 'c', 7: 'g', 4: 'd', 9: 'i', 1: 'a',
-                         6: 'f', 2: 'b', 8: 'h', 5: 'e', 10: 'j'}
-        array = [i for i in sort_nums(dict_scrambled)]
-        self.assertEqual(array, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
-
-
-    def test_big(self):
-        dict_big = {i: 'a' for i in range(10000)}
-        del dict_big[0]
-        array = [i for i in sort_nums(dict_big)]
-        self.assertEqual(array, ['a' for i in range(9999)])
-
-
-    def test_small(self):
-        array = [i for i in sort_nums({1: 'a'})]
-        self.assertEqual(array, ['a'])
+        array = [i for i in sort_nums(test_dict)]
+        self.assertEqual(array, chars)
 
 
 
