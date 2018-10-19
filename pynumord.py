72f1ae6e3ab771cbd285f43482c4598aef35aca4
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-19 10:46:26 -0400

    Simplified test

--- pynumord.py
+++ pynumord.py
@@ -4,11 +4,10 @@ from hypothesis import given, assume, settings
 import hypothesis.strategies as st
 
 def sort_nums(incoming):
+    """side effect: deletes incoming as it processes it"""
     sought = 1
     store_early = {}
-    print("New!!!")
     while len(incoming) > 0:
-        print("Top")
         key, value = incoming.popitem()
         if key == sought:
             yield value
@@ -19,7 +18,6 @@ def sort_nums(incoming):
 
         else:
             store_early[key] = value
-            print("Stored!")
 
 
 
@@ -27,7 +25,6 @@ def sort_nums(incoming):
 def check_store(seek, store):
     while seek in store:
         found = store[seek]
-        print("Check!")
         del store[seek]
         seek += 1
         yield found, seek
@@ -35,33 +32,20 @@ def check_store(seek, store):
 
 
 class TestOrders(unittest.TestCase):
-    @settings(deadline=None, timeout=300, suppress_health_check = hypothesis.HealthCheck.all())
-    @given(value_list = st.lists(st.characters()))  # how can I scramble the dict entries, while knowing what order I want them out in?
-    def test_sorting(self, value_list):
-        test_dict = dict(zip(range(len(value_list)), value_list))
-        if test_dict:
-            del test_dict[0]
-            del value_list[0]
+    @settings(deadline=None, timeout=hypothesis.unlimited, suppress_health_check = hypothesis.HealthCheck.all())
+    @given(test_dict = st.dictionaries(st.integers(min_value = 1),
+                                        st.characters()))
+    def test_sorting(self, test_dict):
+        for i in range(len(test_dict) + 1):
+            assume(i == 0 or i in test_dict)
 
-        if test_dict:
-            @given(order_dict_out = st.lists(st.integers(min_value = 1, max_value = len(test_dict)), 
-                                            min_size = len(test_dict), 
-                                            max_size = len(test_dict),
-                                            unique = True))
-            def test_orders(self, order_dict_out):
-                assume(i == 0 or i in order_dict_out for i in range(len(test_dict)))
-                scrambled_dict = {}
-                for key in order_dict_out:  # create a dictionary identical to test_dict, but entering key/value pairs in random order
-                    scrambled_dict[key] = test_dict[key]
-            
-                array = [i for i in sort_nums(scrambled_dict)]
-                self.assertEqual(array, value_list)
-
-
-            test_orders(self)
+        copy_of_test_dict = test_dict.copy()
+        ordered_values = [i for i in sort_nums(test_dict)]
+        if len(copy_of_test_dict) > 0:
+            comparison_list = [copy_of_test_dict[i+1] for i in range(len(copy_of_test_dict))]
+            self.assertEqual(ordered_values, comparison_list)
         else:
-            array = [i for i in sort_nums(test_dict)]
-            self.assertEqual(array, value_list)
+            self.assertEqual(ordered_values, [])
 
 
 
